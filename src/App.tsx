2744055a6fc5f365a8eb6be9574de6f535c9652a
idcetim <javirofe96@gmail.com>
2024-09-28 07:43:58 +0200

    Add tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,32 @@ import enMessages from './locales/en.json'
 import espMessages from './locales/es.json'
 import { useSelector } from 'react-redux'
 import { IntlProvider } from 'react-intl'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import './App.css'
+
 
 export const ModalContext = createContext<ModalContextType | undefined>(undefined)
 
 function App() {
+  const queryClient = new QueryClient()
   const language: Language = useSelector((s: any) => s.language)
   const [showModal, setShowModal] = useState<boolean>(false)
 
   return (
-    <IntlProvider locale={language === 'en' ? 'en' : 'es'} messages={language === 'en' ? enMessages : espMessages}>
-      <ModalContext.Provider value={{ showModal, setShowModal }}>
-        <main>
-          <BrowserRouter>
-            <Routes>
-              <Route path={AppRoutes.Home} element={<Home />} />
-            </Routes>
-          </BrowserRouter>
-        </main>
-      </ModalContext.Provider>
-    </IntlProvider>
+    <QueryClientProvider client={queryClient}>
+      <IntlProvider locale={language === 'en' ? 'en' : 'es'} messages={language === 'en' ? enMessages : espMessages}>
+        <ModalContext.Provider value={{ showModal, setShowModal }}>
+          <main>
+            <BrowserRouter>
+              <Routes>
+                <Route path={AppRoutes.Home} element={<Home />} />
+              </Routes>
+            </BrowserRouter>
+          </main>
+        </ModalContext.Provider>
+      </IntlProvider>
+    </QueryClientProvider>
   )
 }
 
