21993eaf347e4b0fada411a95d1aa5f7a4317bbe
idcetim <javirofe96@gmail.com>
2024-09-28 07:51:07 +0200

    Add hook to manage current dimensions

---
+++ src/hooks/useCurrentDimensions.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import { MOBILE_MAX_WIDTH } from '../contants'
+
+const useCurrentDimensions = () => {
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  })
+
+  useEffect(() => {
+    function updateResize() {
+      setDimensions({ width: window.innerWidth, height: window.innerHeight })
+    }
+    window.addEventListener('resize', updateResize)
+
+    return () => {
+      window.removeEventListener('resize', updateResize)
+    }
+  }, [dimensions])
+  return {
+    dimensions,
+  }
+}
+
+export default useCurrentDimensions
+
+export const useIsMobile = (): boolean => {
+  const { dimensions } = useCurrentDimensions()
+  return dimensions.width <= MOBILE_MAX_WIDTH
+}
